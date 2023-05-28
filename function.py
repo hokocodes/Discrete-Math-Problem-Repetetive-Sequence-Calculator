a9b3a0d8414c7ea5a4dd053fc5a8c4774f0515d7
hokole <94093120+hokole@users.noreply.github.com>
2023-05-27 20:20:48 -0500

    Added comments

--- function.py
+++ function.py
@@ -3,13 +3,13 @@ def f(start):
     i = 0
 
     while i < 100:
-        if start % 2 == 0:
+        if start % 2 == 0: #if it is even
             y = start/2
-            print(f'{start}' + " / 2 = " + str(y))
+            print(f'{start}' + " / 2 = " + str(y)) #generate (recursively) a sequence of positive integers
             start = y
-        elif start % 2 != 0:
+        elif start % 2 != 0: #if it is odd
             z = start * 3 + 1
-            print(f'{start}' + " * 3 + 1 " + str(z))
+            print(f'{start}' + " * 3 + 1 " + str(z)) #generate (recursively) a sequence of positive integers
             start = z
         i += 1
 
